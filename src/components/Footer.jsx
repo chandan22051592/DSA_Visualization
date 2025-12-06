9ed4b70aabb4300e12a4c160c05332f89cd0db29
testbulksend-tech <testbulksend@gmail.com>
2025-12-06 11:49:17 +0530

    added authorName to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 function Footer(){
     return (
         <footer class="bg-gray-800 text-gray-400 text-center py-6 mt-1">
-            <p>© 2024 LearnWithMe. All rights reserved <span class="text-pink-500 font-semibold">Developer</span></p>
+            <p>© 2024 LearnWithChandan. All rights reserved <span class="text-pink-500 font-semibold">Developer</span></p>
         </footer>
     );
 }
